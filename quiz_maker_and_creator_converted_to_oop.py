7814fe33b6229b95cb737e85d10bf36dc032fc12
gelaiii10 <gelaibacleaan@gmail.com>
2025-05-28 17:19:18 -0800

    added class for questions

--- quiz_maker_and_creator_converted_to_oop.py
+++ quiz_maker_and_creator_converted_to_oop.py
@@ -1,4 +1,10 @@
+import random
+
 # create a class for questions
+class Question:
+    def __init__(self, question_text, correct_answer):
+        self.question_text = question_text
+        self.correct_answer = correct_answer
 # check if the user's answer is correct.
 # create a class for quiz
 # load questions from a file
