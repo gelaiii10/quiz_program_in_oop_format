66a1794d97a2d2f562d78c3394e98b0afc4888b7
gelaiii10 <gelaibacleaan@gmail.com>
2025-05-28 17:51:05 -0800

    load questions from file

--- quiz_maker_and_creator_converted_to_oop.py
+++ quiz_maker_and_creator_converted_to_oop.py
@@ -14,5 +14,13 @@ class Question:
 class Quiz:
     def __init__(self, filename):
         self.questions = self.load_questions(filename)
-# load questions from a file
+    
+    def load_questions(self, filename):
+        # load questions from a file
+        questions = []
+        with open(filename, 'r') as file:
+            for line in file:
+                question_text, correct_answer = line.strip().split(';')
+                questions.append(Question(question_text, correct_answer))
+        return questions
 # conduct the quiz with the user
