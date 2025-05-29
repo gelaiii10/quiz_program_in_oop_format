2b221e5ac0eaf6459895972429280a9da0def0a2
gelaiii10 <gelaibacleaan@gmail.com>
2025-05-28 17:25:13 -0800

    check the users answer

--- quiz_maker_and_creator_converted_to_oop.py
+++ quiz_maker_and_creator_converted_to_oop.py
@@ -5,7 +5,10 @@ class Question:
     def __init__(self, question_text, correct_answer):
         self.question_text = question_text
         self.correct_answer = correct_answer
-# check if the user's answer is correct.
+
+    def check_answer(self, user_answer):
+        # check if the user's answer is correct.
+        return user_answer.strip().lower() == self.correct_answer.lower()
 # create a class for quiz
 # load questions from a file
 # conduct the quiz with the user
